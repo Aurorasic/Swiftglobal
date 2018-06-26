d9cc1af44c3f597d8e2d9ceaded42d2a1d2fc413
Brady99 <99beyond99@gmail.com>
2018-06-26 10:36:49 +0800

    Adding ScoreDaoService Junit tests

---
+++ blockchain/blockchain-app/src/test/java/com/higgsblock/global/chain/app/BaseMockTest.java
@@ -0,0 +1,14 @@
+package com.higgsblock.global.chain.app;
+
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+/**
+ * @author Su Jiulong
+ * @date 2018-06-25
+ */
+@RunWith(PowerMockRunner.class)
+@PowerMockIgnore("javax.management.*")
+public class BaseMockTest {
+}
