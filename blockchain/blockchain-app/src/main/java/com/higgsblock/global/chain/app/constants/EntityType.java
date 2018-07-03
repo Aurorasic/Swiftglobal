59adf3cdde771c212f6ebe0748c1aa7b793905ed
yuanjiantao <77282491@qq.com>
2018-07-03 17:59:08 +0800

    add source block request entity type

--- blockchain/blockchain-app/src/main/java/com/higgsblock/global/chain/app/constants/EntityType.java
+++ blockchain/blockchain-app/src/main/java/com/higgsblock/global/chain/app/constants/EntityType.java
@@ -25,6 +25,7 @@ public enum EntityType {
     GET_BLOCK("203"),
     GET_MAX_HEIGHT("204"),
     SOURCE_BLOCK("205"),
+    SOURCE_BLOCK_REQ("207"),
     CANDIDATE_BLOCK("206"),
     CANDIDATE_BLOCK_HASH("208"),
 
