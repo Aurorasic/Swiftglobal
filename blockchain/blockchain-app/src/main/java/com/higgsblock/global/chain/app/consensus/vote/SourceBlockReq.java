85d5dfe5034f82266171d8917aab5703bd664244
yuanjiantao <77282491@qq.com>
2018-07-03 17:38:08 +0800

    add source block request

---
+++ blockchain/blockchain-app/src/main/java/com/higgsblock/global/chain/app/consensus/vote/SourceBlockReq.java
@@ -0,0 +1,24 @@
+package com.higgsblock.global.chain.app.consensus.vote;
+
+import com.higgsblock.global.chain.app.common.message.Message;
+import com.higgsblock.global.chain.app.constants.EntityType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Set;
+
+/**
+ * @author yuanjiantao
+ * @date 7/2/2018
+ */
+@Message(EntityType.SOURCE_BLOCK)
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Slf4j
+public class SourceBlockReq {
+
+    private Set<String> blockHashs;
+}
