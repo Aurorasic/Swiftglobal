39957b69c042098c3715c0b583c7f9bb0e980995
baizhengwen <baizhengwen@primeledger.cn>
2018-07-04 17:59:32 +0800

    update config

--- blockchain/blockchain-app/src/main/java/com/higgsblock/global/chain/app/Application.java
+++ blockchain/blockchain-app/src/main/java/com/higgsblock/global/chain/app/Application.java
@@ -4,7 +4,6 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.higgsblock.global.chain.app.context.AppContext;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ApplicationContext;
@@ -19,11 +18,6 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan({"com.higgsblock.global.chain"})
 public class Application {
 
-    public static final int PRE_BLOCK_COUNT = 1;
-
-    @Autowired
-    private AppContext appContext;
-
     public static void main(String[] args) throws Exception {
         // Cyclic reference detection
         JSON.DEFAULT_GENERATE_FEATURE |= SerializerFeature.DisableCircularReferenceDetect.getMask();
@@ -35,11 +29,7 @@ public class Application {
         JSON.DEFAULT_GENERATE_FEATURE |= SerializerFeature.MapSortField.getMask();
 
         ApplicationContext springContext = SpringApplication.run(Application.class, args);
-        Application application = springContext.getBean(Application.class);
-        application.start();
-    }
-
-    private void start() throws Exception {
+        AppContext appContext = springContext.getBean(AppContext.class);
         appContext.start();
     }
 }
