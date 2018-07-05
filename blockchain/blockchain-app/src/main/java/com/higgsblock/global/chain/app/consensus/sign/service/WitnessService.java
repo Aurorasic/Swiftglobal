3559cbac91c8dbe778e8aba6de6e76e72e191539
deke1521 <deke1521@163.com>
2018-07-05 13:03:13 +0800

    modify log info

--- blockchain/blockchain-app/src/main/java/com/higgsblock/global/chain/app/consensus/sign/service/WitnessService.java
+++ blockchain/blockchain-app/src/main/java/com/higgsblock/global/chain/app/consensus/sign/service/WitnessService.java
@@ -438,7 +438,7 @@ public class WitnessService {
             String voteSign = vote.getSignature();
             if (StringUtils.isBlank(bestBlockHash)) {
                 bestBlockHash = voteBlockHash;
-                LOGGER.info("height {},version {},the version is {},set the bestBlockHash to {}", voteHeight, version, bestBlockHash);
+                LOGGER.info("height {},version {} ,set the bestBlockHash to {}", voteHeight, version, bestBlockHash);
             } else {
                 bestBlockHash = bestBlockHash.compareTo(voteBlockHash) < 0 ? voteBlockHash : bestBlockHash;
                 LOGGER.info("height {},version {},change the bestBlockHash to {}", voteHeight, version, bestBlockHash);
