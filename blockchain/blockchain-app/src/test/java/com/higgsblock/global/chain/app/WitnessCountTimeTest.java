7220d8627b92d626a5a209cb098a8af785445d77
yezaiyongprimeledger <yezaiyong@primeledger.cn>
2018-07-05 10:57:28 +0800

    update code style

--- blockchain/blockchain-app/src/test/java/com/higgsblock/global/chain/app/WitnessCountTimeTest.java
+++ blockchain/blockchain-app/src/test/java/com/higgsblock/global/chain/app/WitnessCountTimeTest.java
@@ -1,13 +1,11 @@
-package com.higgsblock.global.chain.app.blockchain;
+package com.higgsblock.global.chain.app;
 
-import com.higgsblock.global.chain.app.blockchain.transaction.TransactionService;
-import com.higgsblock.global.chain.common.enums.SystemCurrencyEnum;
+import com.higgsblock.global.chain.app.api.service.UTXORespService;
+import com.higgsblock.global.chain.app.blockchain.Block;
+import com.higgsblock.global.chain.app.blockchain.BlockService;
 import com.higgsblock.global.chain.common.utils.ExecutorServices;
-import com.higgsblock.global.chain.network.PeerManager;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Service;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -16,12 +14,10 @@ import java.util.concurrent.TimeUnit;
  * @program: HiggsGlobal
  * @description:
  * @author: yezaiyong
- * @create: 2018-06-29 20:32
+ * @create: 2018-07-02 15:01
  **/
-@Service
-@Scope("prototype")
 @Slf4j
-public class WitnessCountTime {
+public class WitnessCountTimeTest {
 
     private static long preHeight = 0;
     private static long currHeight = 0;
@@ -30,48 +26,38 @@ public class WitnessCountTime {
     private volatile boolean isRunning;
     static Block block;
     public volatile static boolean isCurrBlockConfirm = false;
-    public static final long WAIT_WITNESS_TIME = 540;
 
+    @Autowired
+    private UTXORespService utxoRespService;
 
     @Autowired
     private BlockService blockService;
-    @Autowired
-    private PeerManager peerManager;
-    @Autowired
-    private TransactionService transactionService;
 
-    public boolean queryCurrHeightStartTime() throws InterruptedException {
-        String address = peerManager.getSelf().getId();
-        if (BlockService.WITNESS_ADDRESS_LIST.contains(address)) {
-            currHeight = blockService.getMaxHeight();
-            return startTimer();
-        }
-        return false;
+
+    public boolean queryCurrHeight() throws InterruptedException {
+        currHeight = currHeight;//blockService.getBestMaxHeight();
+        return startTimer();
     }
 
-    public static boolean isCurrBlockConfirm(Block block) {
-        try {
-            WitnessCountTime.block = block;
-            TimeUnit.SECONDS.sleep(3);
-            return isCurrBlockConfirm;
-        } catch (InterruptedException e) {
-            LOGGER.error("isCurrBlockConfirm error {}", e);
-        }
-        return false;
+    public static boolean isCurrBlockConfirm(Block block) throws InterruptedException {
+        WitnessCountTimeTest.block = block;
+        TimeUnit.SECONDS.sleep(5);
+        return isCurrBlockConfirm;
     }
 
-    public static void instantiationBlock(Block block) {
-        WitnessCountTime.block = null;
-        WitnessCountTime.curSec = 0;
+    public static void instantiateiBlock(Block block) {
+        WitnessCountTimeTest.block = null;
+        WitnessCountTimeTest.curSec = 0;
         preHeight = block.getHeight();
         currHeight = block.getHeight();
+        ;
     }
 
+
     public boolean startTimer() throws InterruptedException {
-        //If the received block is not empty, it is proved to be in the receiving block, and empty indicates that the timer needs to start
         if (block == null) {
             preHeight = currHeight;
-            WitnessCountTime.curSec = 0;
+            this.curSec = 0;
             if (executorService == null) {
                 this.start(ExecutorServices.newSingleThreadExecutor(getClass().getName(), 1));
             }
@@ -86,42 +72,37 @@ public class WitnessCountTime {
                 while (isRunning) {
                     try {
                         ++curSec;
-                        LOGGER.info("curSec = " + curSec);
+                        LOGGER.info("-------------" + curSec);
                         TimeUnit.SECONDS.sleep(1);
                         if (block == null) {
                             if (preHeight >= currHeight) {
                                 LOGGER.info("block is null  pre >= curr;pre = " + preHeight + " curr =" + currHeight);
                             } else {
                                 preHeight = currHeight;
-                                WitnessCountTime.curSec = 0;
+                                this.curSec = 0;
                                 LOGGER.info("block is null  pre < curr;pre = " + preHeight + " curr =" + currHeight);
                             }
                         } else {
                             if (preHeight >= block.getHeight()) {
-                                //need to wait for the block to come and continue timing. The current block is an invalid block
                                 isCurrBlockConfirm = false;
                                 LOGGER.info("block is not null  pre >= block height ;pre = " + preHeight + " block height =" + block.getHeight());
+                                //this.block = null;
                             } else {
-                                if (curSec >= WAIT_WITNESS_TIME) {
-                                    LOGGER.info("time of arrival  accept common miner block or candidate miner block");
-                                    // can receive either a reserve or a normal reserve
+                                if (curSec >= 100) {
                                     isCurrBlockConfirm = true;
                                     //WitnessCountTime.curSec =0;
                                     //preHeight = block.getHeight();
                                 } else {
-                                    //Only ordinary mining areas can be accepted
-                                    if (verifyBlockBelongCommonMiner(block)) {
+                                    if (verifyBlock(block)) {
+                                        //this.block = null;
                                         isCurrBlockConfirm = false;
-                                        LOGGER.info("time of no arrival,only accept common miner block , but this candidate miner block");
                                     } else {
-                                        LOGGER.info("time of no arrival,only accept common miner block , this block is common miner {} ", block.getHash());
                                         isCurrBlockConfirm = true;
                                         //WitnessCountTime.curSec =0;
                                         //preHeight = block.getHeight();
-                                        //currHeight = preHeight;
                                     }
                                 }
-                                LOGGER.info("block is not null  pre < block height ;pre = " + preHeight + " block height =" + block.getHeight() + "is accept this block =" + isCurrBlockConfirm);
+                                LOGGER.info("block is not null  pre < block height ;pre = " + preHeight + " block height =" + block.getHeight() + "是否应该接收该区块=" + isCurrBlockConfirm);
                             }
                         }
                     } catch (InterruptedException e) {
@@ -134,7 +115,21 @@ public class WitnessCountTime {
 
     }
 
-    public boolean verifyBlockBelongCommonMiner(Block block) {
-        return transactionService.hasStake(block.getMinerFirstPKSig().getAddress(), SystemCurrencyEnum.CMINER);
+    public boolean verifyBlock(Block block) {
+
+        return true;
+//        List<UTXO> list =utxoRespService.getUTXOsByAddress(block.getMinerFirstPKSig().getAddress());
+//        boolean isCMINER =false;
+//        if (list !=null){
+//            for (UTXO utxo : list){
+//                String currency=utxo.getOutput().getMoney().getCurrency();
+//                if (currency.equals(SystemCurrencyEnum.CMINER)){
+//                    isCMINER =true;
+//                    continue;
+//                }
+//            }
+//        }
+//        return isCMINER;
     }
+
 }
