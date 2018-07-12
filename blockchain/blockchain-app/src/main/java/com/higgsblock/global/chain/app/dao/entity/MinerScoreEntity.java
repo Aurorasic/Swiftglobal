da05dfb67a7b6dbf90d4b8784246f149fe221161
wangxiangyi <wangxiangyi@primeledger.cn>
2018-07-12 12:36:09 +0800

    entity support jpa

--- blockchain/blockchain-app/src/main/java/com/higgsblock/global/chain/app/dao/entity/MinerScoreEntity.java
+++ blockchain/blockchain-app/src/main/java/com/higgsblock/global/chain/app/dao/entity/MinerScoreEntity.java
@@ -4,6 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * @author yangshenghong
  * @date 2018-05-07
@@ -11,8 +15,12 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "t_score")
 public class MinerScoreEntity {
+    @Column
     private String address;
+    @Column
     private int score;
 
     public MinerScoreEntity(String address) {
--- blockchain/blockchain-app/src/main/java/com/higgsblock/global/chain/app/dao/entity/PeerEntity.java
+++ blockchain/blockchain-app/src/main/java/com/higgsblock/global/chain/app/dao/entity/PeerEntity.java
@@ -2,18 +2,28 @@ package com.higgsblock.global.chain.app.dao.entity;
 
 import lombok.Data;
 
+import javax.persistence.Column;
+
 /**
  * @author yangshenghong
  * @date 2018-05-07
  */
 @Data
 public class PeerEntity {
+    @Column(name = "pub_key")
     private String pubKey;
+    @Column(name = "id")
     private String id;
+    @Column(name = "ip")
     private String ip;
+    @Column(name = "socket_port")
     private int socketPort;
+    @Column(name = "http_port")
     private int httpPort;
+    @Column(name = "version")
     private int version;
+    @Column(name = "signature")
     private String signature;
+    @Column(name = "retry")
     private int retry;
 }
