dede695e299c0c03e6c1301104e16cd2222b9fbd
huangshengli <huangshengli@primeledger.cn>
2018-07-19 21:15:35 +0800

    persist block add transaction

--- blockchain/blockchain-app/src/main/java/com/higgsblock/global/chain/app/service/impl/BlockService.java
+++ blockchain/blockchain-app/src/main/java/com/higgsblock/global/chain/app/service/impl/BlockService.java
@@ -12,7 +12,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -147,7 +146,6 @@ public class BlockService implements IBlockService {
      * 5.save score
      * 6.save new dpos
      **/
-    @Transactional(rollbackFor = Exception.class)
     @Override
     public void saveBlock(Block block) throws Exception {
 
