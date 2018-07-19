0b195c9ac2970b954ec15fa9b96ec41dccebf5e3
yuanjiantao <77282491@qq.com>
2018-07-19 12:57:07 +0800

    refactor code

---
+++ blockchain/blockchain-app/src/main/java/com/higgsblock/global/chain/app/api/outer/ManagerApi.java
@@ -0,0 +1,32 @@
+package com.higgsblock.global.chain.app.api.outer;
+
+import com.higgsblock.global.chain.app.api.vo.ResponseData;
+import com.higgsblock.global.chain.app.blockchain.DataErrorProcessor;
+import com.higgsblock.global.chain.app.blockchain.transaction.Transaction;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * The browser accesses the whitelist filter
+ *
+ * @author kongyu
+ * @date 2018-5-21 16:20
+ */
+@RequestMapping("/v1.0.0/manager")
+@RestController
+@Slf4j
+public class ManagerApi {
+
+    @Autowired
+    private DataErrorProcessor dataErrorProcessor;
+
+
+    @RequestMapping("/reimportData")
+    public ResponseData<Boolean> sendTx(@RequestBody Transaction tx) {
+        dataErrorProcessor.handleError();
+        return ResponseData.success(true);
+    }
+}
