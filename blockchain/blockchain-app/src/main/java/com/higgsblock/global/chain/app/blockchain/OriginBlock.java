ec61f3125b2b3105f5adcb5171ee8c384b5123f6
deke1521 <deke1521@163.com>
2018-07-19 14:24:14 +0800

    add OriginBlock

---
+++ blockchain/blockchain-app/src/main/java/com/higgsblock/global/chain/app/blockchain/OriginBlock.java
@@ -0,0 +1,23 @@
+package com.higgsblock.global.chain.app.blockchain;
+
+import com.higgsblock.global.chain.app.common.constants.MessageType;
+import com.higgsblock.global.chain.app.common.message.Message;
+import com.higgsblock.global.chain.common.entity.BaseSerializer;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author yangyi
+ * @deta 2018/7/19
+ * @description
+ */
+@Message(MessageType.ORIGIN_BLOCK)
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Slf4j
+public class OriginBlock extends BaseSerializer {
+    private Block block;
+}
