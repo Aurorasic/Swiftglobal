deb386f601841f1fa9b5d9d583c345857847dc39
kongyu <kongyu@primeledger>
2018-07-24 11:15:33 +0800

    update log

--- blockchain/blockchain-app/src/main/java/com/higgsblock/global/chain/app/task/GuarderTask.java
+++ blockchain/blockchain-app/src/main/java/com/higgsblock/global/chain/app/task/GuarderTask.java
@@ -85,7 +85,7 @@ public class GuarderTask extends BaseTask implements IEventBusListener {
             for (String blockHash : maxBlockIndex.getBlockHashs()) {
                 String address = peerManager.getSelf().getId();
                 if (!blockChainService.isGuarder(address, blockHash)) {
-                    LOGGER.warn("this miner no guarder currency");
+                    LOGGER.warn("this miner no guarder currency, address={}, block hash={}", address, blockHash);
                     return;
                 }
                 LOGGER.info("begin to packageNewBlock,height={},preBlcokHash={},this guarder address ={}", expectHeight, blockHash, address);
