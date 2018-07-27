5a7b6110bc5b847764a7ff0f430b85013f629ca2
yanghuadong <yanghuadong@primeledger.cn>
2018-07-27 18:33:43 +0800

    clean code

--- blockchain/blockchain-app/src/main/java/com/higgsblock/global/chain/app/sync/handler/BlockResponseHandler.java
+++ blockchain/blockchain-app/src/main/java/com/higgsblock/global/chain/app/sync/handler/BlockResponseHandler.java
@@ -13,7 +13,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.List;
