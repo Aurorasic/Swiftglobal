32b0615dee2aa171e74977c915b4e180907a3f4b
yanghuadong <yanghuadong@primeledger.cn>
2018-08-01 20:54:46 +0800

    format code

--- blockchain/blockchain-app/src/main/java/com/higgsblock/global/chain/app/service/ITransactionService.java
+++ blockchain/blockchain-app/src/main/java/com/higgsblock/global/chain/app/service/ITransactionService.java
@@ -7,22 +7,61 @@ import com.higgsblock.global.chain.common.enums.SystemCurrencyEnum;
 import java.util.Set;
 
 /**
+ * The interface Transaction service.
+ *
  * @description:
  * @author: yezaiyong
- * @create: 2018-07-21 12:38
- **/
+ * @create: 2018 -07-21 12:38
+ */
 public interface ITransactionService {
 
-    public boolean validTransactions(Block block);
+    /**
+     * Valid transactions boolean.
+     *
+     * @param block the block
+     * @return the boolean
+     */
+    boolean validTransactions(Block block);
 
+    /**
+     * Received transaction.
+     *
+     * @param tx the tx
+     */
+    void receivedTransaction(Transaction tx);
 
-    public void receivedTransaction(Transaction tx);
+    /**
+     * Has stake boolean.
+     *
+     * @param address  the address
+     * @param currency the currency
+     * @return the boolean
+     */
+    boolean hasStake(String address, SystemCurrencyEnum currency);
 
-    public boolean hasStake(String address, SystemCurrencyEnum currency);
+    /**
+     * Has stake boolean.
+     *
+     * @param preBlockHash the pre block hash
+     * @param address      the address
+     * @param currency     the currency
+     * @return the boolean
+     */
+    boolean hasStake(String preBlockHash, String address, SystemCurrencyEnum currency);
 
-    public boolean hasStake(String preBlockHash, String address, SystemCurrencyEnum currency);
+    /**
+     * Gets removed miners.
+     *
+     * @param tx the tx
+     * @return the removed miners
+     */
+    Set<String> getRemovedMiners(Transaction tx);
 
-    public Set<String> getRemovedMiners(Transaction tx);
-
-    public Set<String> getAddedMiners(Transaction tx);
+    /**
+     * Gets added miners.
+     *
+     * @param tx the tx
+     * @return the added miners
+     */
+    Set<String> getAddedMiners(Transaction tx);
 }
