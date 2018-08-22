3d620c4cab51d1aba6a2f6e321ea7877edbe52fc
baizhengwen <baizhengwen@primeledger.cn>
2018-08-22 15:54:17 +0800

    clean code

--- blockchain/blockchain-app/src/main/java/com/higgsblock/global/chain/app/api/inner/RegistryController.java
+++ blockchain/blockchain-app/src/main/java/com/higgsblock/global/chain/app/api/inner/RegistryController.java
@@ -5,7 +5,6 @@ import com.higgsblock.global.chain.app.net.peer.PeerManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -46,10 +45,5 @@ public class RegistryController {
 
         peerManager.add(peer);
         return true;
-    }
-
-    @RequestMapping("/query")
-    public Peer query(@RequestParam String address) {
-        return peerManager.getById(address);
     }
 }
