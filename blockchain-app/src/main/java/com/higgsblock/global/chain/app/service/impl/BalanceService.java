8054607b0d4fd725faf13fc2d0022a3c798bd1ac
yezaiyong <yezaiyong@primeledger.cn>
2018-09-27 10:53:46 +0800

    update balance info

--- blockchain-app/src/main/java/com/higgsblock/global/chain/app/service/impl/BalanceService.java
+++ blockchain-app/src/main/java/com/higgsblock/global/chain/app/service/impl/BalanceService.java
@@ -51,11 +51,11 @@ public class BalanceService implements IBalanceService {
     @Override
     public Money getBalanceOnBest(String address, String currency) {
         Map<String, Money> map = get(address);
-        if (MapUtils.isNotEmpty(map) && map.containsKey(currency)) {
-            return new Money("0");
+        Money result = map.get(currency);
+        if (result == null) {
+            return new Money(0, currency);
         }
-
-        return map.get(currency);
+        return result;
     }
 
     /**
