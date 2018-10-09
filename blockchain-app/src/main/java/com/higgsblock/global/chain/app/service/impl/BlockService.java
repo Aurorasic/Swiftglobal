15d35131159b29d357503082f55f3e055feb1d32
zhaoxiaogang <zhaoxiaogang@primeledger.cn>
2018-10-09 11:05:20 +0800

    update BlockService.java

--- blockchain-app/src/main/java/com/higgsblock/global/chain/app/service/impl/BlockService.java
+++ blockchain-app/src/main/java/com/higgsblock/global/chain/app/service/impl/BlockService.java
@@ -111,6 +111,8 @@ public class BlockService implements IBlockService {
     private DefaultSystemProperties systemProperties;
     @Autowired
     private TransactionService transactionService;
+    @Autowired
+    private IBlockChainInfoService blockChainInfoService;
 
     private Cache<String, Block> blockCache = Caffeine.newBuilder().maximumSize(LRU_CACHE_SIZE).build();
 
@@ -666,6 +668,7 @@ public class BlockService implements IBlockService {
             Block newBestBlock = null;
             if (isFirst) {
                 newBestBlock = getToBeBestBlock(block);
+                blockChainInfoService.setMaxHeight(block.getHeight());
             } else {
                 LOGGER.info("block:{} is not first at height :{}", block.getHash(), block.getHeight());
             }
