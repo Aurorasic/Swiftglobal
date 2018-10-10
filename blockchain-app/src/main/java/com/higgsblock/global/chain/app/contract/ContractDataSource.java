90257cd15e6cad78130d3b96e42dbe9856129617
zhaoxiaogang <zhaoxiaogang@primeledger.cn>
2018-10-10 14:18:21 +0800

    add test and log for contractdatasrouce;

--- blockchain-app/src/main/java/com/higgsblock/global/chain/app/contract/ContractDataSource.java
+++ blockchain-app/src/main/java/com/higgsblock/global/chain/app/contract/ContractDataSource.java
@@ -4,16 +4,18 @@ import com.higgsblock.global.chain.app.dao.IContractRepository;
 import com.higgsblock.global.chain.app.dao.entity.ContractEntity;
 import com.higgsblock.global.chain.vm.datasource.DbSettings;
 import com.higgsblock.global.chain.vm.datasource.DbSource;
+import lombok.extern.slf4j.Slf4j;
 import org.spongycastle.util.encoders.Hex;
 
 import java.util.Map;
 import java.util.Set;
 
+@Slf4j
 public class ContractDataSource implements DbSource<byte[]> {
-    private IContractRepository iContractRepository;
+    private IContractRepository contractRepository;
 
-    public ContractDataSource(IContractRepository iContractRepository) {
-        this.iContractRepository = iContractRepository;
+    public ContractDataSource(IContractRepository contractRepository) {
+        this.contractRepository = contractRepository;
     }
 
     @Override
@@ -63,17 +65,18 @@ public class ContractDataSource implements DbSource<byte[]> {
 
 
     private void updateBatchInternal(Map<byte[], byte[]> rows)  {
+        LOGGER.info("==updateBatchInternal=====");
         for (Map.Entry<byte[], byte[]> entry : rows.entrySet()) {
             if (entry.getValue() == null) {
                 String strKey = Hex.toHexString(entry.getKey());
-                iContractRepository.delete(strKey);
+                contractRepository.delete(strKey);
             } else {
                 System.out.println("key===" + Hex.toHexString(entry.getKey()) +
                         "--- value : " + Hex.toHexString(entry.getValue()));
                 String strKey = Hex.toHexString(entry.getKey());
                 String strValue = Hex.toHexString(entry.getValue());
                 ContractEntity entity = new ContractEntity(strKey, strValue);
-                iContractRepository.save(entity);
+                contractRepository.save(entity);
             }
         }
     }
@@ -91,7 +94,7 @@ public class ContractDataSource implements DbSource<byte[]> {
     @Override
     public byte[] get(byte[] key) {
         String strKey = Hex.toHexString(key);
-        ContractEntity entity = iContractRepository.findOne(strKey);
+        ContractEntity entity = contractRepository.findOne(strKey);
 
         if (entity != null) {
             return Hex.decode(entity.getValue());
