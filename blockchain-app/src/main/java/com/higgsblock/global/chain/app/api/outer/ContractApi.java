f5cdfe95e1b23dbb2549e2d7ce53c41851ab96ac
chenjiaweiprimeledger <chenjiawei@primeledger.cn>
2018-10-29 15:42:35 +0800

    add an api.

--- blockchain-app/src/main/java/com/higgsblock/global/chain/app/api/outer/ContractApi.java
+++ blockchain-app/src/main/java/com/higgsblock/global/chain/app/api/outer/ContractApi.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 /**
  * @author tangkun
  * @date 2018-10-24
@@ -27,6 +29,17 @@ public class ContractApi {
         ContractEntity account = contractRepository.findOne(key);
         if (account != null) {
             return ResponseData.success(account.getValue());
+        }
+
+        return ResponseData.failure(RespCodeEnum.FAILED);
+    }
+
+    @RequestMapping("/list")
+    public ResponseData<List<ContractEntity>> sendTxs() {
+
+        List<ContractEntity> account = contractRepository.findAll();
+        if (account != null) {
+            return ResponseData.success(account);
         }
 
         return ResponseData.failure(RespCodeEnum.FAILED);
